d23a24257729994fc35ae07ea1eacadc986d6f36
Noah Jacobson <naj32@njit.edu>
2024-02-13 19:25:22 -0500

    feat: add early frontend code

--- frontend/src/login.php
+++ frontend/src/login.php
@@ -1,21 +1,46 @@
 <?php
 
+require_once('includes/common/rabbitMQLib.inc.php');
+require_once('includes/common/types.inc.php');
 
-if (!isset($_POST))
-{
-	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
-	echo json_encode($msg);
-	exit(0);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+  $password = filter_input(INPUT_POST, 'password');
+
+  function attemptLogin($email, $password) {
+    $client = new rabbitMQClient("includes/rabbitmq.ini");
+    $request = new JAND\FrontendRequest(JAND\FrontendRequests::login);
+    $request->set_email($email);
+    $request->set_password($password);
+    echo(serialize($request));
+    $response = $client->send_request(serialize($request), 'application/php-serialized');
+    echo($response);
+  }
+
+  attemptLogin($email, $password);
 }
-$request = $_POST;
-$response = "unsupported request type, politely FUCK OFF";
-switch ($request["type"])
-{
-	case "login":
-		$response = "login, yeah we can do that";
-	break;
-}
-echo json_encode($response);
-exit(0);
 
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JAND Travel</title>
+  </head>
+  <body>
+    <form method="post">
+      <table>
+        <tr>
+          <th><label for="email">Email:</label></th>
+          <td><input type="email" name="email" id="email" required></td>
+        </tr>
+        <tr>
+          <th><label for="password">Password:</label></th>
+          <td><input type="password" name="password" id="password" required></td>
+        </tr>
+        <tr>
+          <td colspan="2"><input type="submit"></td>
+        </tr>
+      </table>      
+    </form>
+  </body>
+</html>
