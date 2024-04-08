3dd44b027abd7821a85ac4293fabc06ba4a31c67
Noah Jacobson <naj32@njit.edu>
2024-04-08 04:02:22 -0400

    feat: add deployment with DB

--- deploy/server/schemas/jand_deploy.sql
+++ deploy/server/schemas/jand_deploy.sql
@@ -12,17 +12,8 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON jand_deploy.* TO 'jand'@'localhost';
 SET @@sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
 CREATE TABLE bundles (
-    bundle_name VARCHAR(255),
-    version_number INT,
-    status VARCHAR(255),
+    bundle_name VARCHAR(20) NOT NULL,
+    version_number INT NOT NULL,
+    status INT NOT NULL,
     PRIMARY KEY (bundle_name, version_number)
-);
-
-CREATE TABLE bundles_links (
-    bundle_a_name VARCHAR(255),
-    bundle_a_version INT,
-    bundle_b_name VARCHAR(255),
-    bundle_b_version INT,
-    FOREIGN KEY (bundle_a_name, bundle_a_version) REFERENCES bundles(bundle_name, version_number),
-    FOREIGN KEY (bundle_b_name, bundle_b_version) REFERENCES bundles(bundle_name, version_number)
 );
